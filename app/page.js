3bbf9371c633a5469f3ebdfa6a884a8126c87f16
SwiftByte6 <rohitsoneji6@gmail.com>
2025-08-26 22:56:39 +0530

    first commit

---
+++ app/page.js
@@ -0,0 +1,11 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+  <>
+  <div className="">
+    <h1 className="text-5xl font-bold">Lets start...!!!!</h1>
+  </div>
+  </>
+  );
+}
