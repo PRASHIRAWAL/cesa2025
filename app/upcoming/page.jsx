576626c7d1d133cbfc0719d9cf025138c7825a49
SwifyByte6 <rohitsoneji6@gmail.com>
2026-01-26 16:34:14 +0530

    added the event swiper

--- app/upcoming/page.jsx
+++ app/upcoming/page.jsx
@@ -8,7 +8,7 @@ const page = () => {
     <div className="relative min-h-screen bg-[#0A0711] py-10 pt-20">
       <Link 
         href="/"
-        className="fixed hidden md:flex top-6 left-6 z-50 rounded-xl bg-[#12091d] border border-[#392e4e] px-4 py-2 text-sm text-white/80 hover:bg-[#1e0f2b] hover:text-white transition  items-center gap-2"
+        className="fixed md:hidden flex top-6 left-6 z-50 rounded-xl bg-[#12091d] border border-[#392e4e] px-4 py-2 text-sm text-white/80 hover:bg-[#1e0f2b] hover:text-white transition  items-center gap-2"
       >
         ← Back to Home
       </Link>
